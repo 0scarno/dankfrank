51d8822def4fc5924c357f38ec2fc62a61fdf5cd
Tom Hawkins <tks.hawkins11@gmail.com>
2020-10-23 17:04:15 +1000

    mmm

--- Scripts/Potion.gd
+++ Scripts/Potion.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 onready var detector = $Detector
 var interactable = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var min_ttc = 3 #ttc = Time To Complete
+export var max_ttc = 6
 
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,13 +17,24 @@ func _ready():
 #	pass
 func interact():
 	if interactable:
+		potion_start()
 		print("interactions")
+		var time = rng.randi_range(min_ttc, max_ttc)
+		yield(get_tree().create_timer(time), "timeout")
+		potion_end()
 
 func _on_Detector_body_entered(body):
 	if body.name == "Player":
 		interactable = true
 
-
 func _on_Detector_body_exited(body):
 	if body.name == "Player":
 		interactable = false
+		
+func potion_start():
+	interactable = false
+	print_debug("Potion Start")
+	
+func potion_end():
+	interactable = true
+	print_debug("Potion End")
