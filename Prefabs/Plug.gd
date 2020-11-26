2cde9760380247ceecd34dfca630cef0d247e48b
Tom Hawkins <tks.hawkins11@gmail.com>
2020-11-26 14:15:42 +1000

    Plug working just need some rope physics

--- Prefabs/Plug.gd
+++ Prefabs/Plug.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+onready var GC = get_node("/root/GameController")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,9 +8,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	connect(job_complete, GC, "on_job_complete")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func interact():
+	
